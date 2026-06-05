120562d1b5a6e8a1af033ec9f7f9b09aa9d43aeb
Sara Manzoor <saramanzoor76@gmail.com>
2026-06-05 07:55:07 +0500

    Add solution for Day 2 - Return Length of Arguments Passed

---
+++ Day2.js
@@ -0,0 +1,12 @@
+
+function createCounter(n) {
+    return function() {
+        return n++;
+    };
+}
+
+// Usage example:
+let counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
