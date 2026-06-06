fc889c548fbd6b3d7b7fecbef3613ba7b6266d68
Sara Manzoor <saramanzoor76@gmail.com>
2026-06-06 15:31:38 +0500

    Day 11: JavaScript problem solved

---
+++ Day11.js
@@ -0,0 +1,16 @@
+//Memorize
+function memoize(fn) {
+    const cache = {};
+    
+    return function(...args) {
+        const key = args.join(',');
+        
+        if (key in cache) {
+            return cache[key];
+        }
+        
+        const result = fn(...args);
+        cache[key] = result;
+        return result;
+    }
+}
