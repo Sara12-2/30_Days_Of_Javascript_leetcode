eb1169b3d732c66df1716c2e867a80d3f9043a43
Sara Manzoor <saramanzoor76@gmail.com>
2026-06-06 15:31:31 +0500

    Day 10: JavaScript problem solved

--- Day10.js
+++ Day10.js
@@ -1,13 +1,46 @@
-//Return Length of Arguments Passed
+//Allow One Function Call
 
-
-function argumentsLength(...args) {
-    return args.length;
+// const once = (fn) => {
+//     let called = false;
+//     return (...args) => called ? undefined : (called = true, fn(...args));
+// };
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function once(fn) {
+    let called = false;
+    
+    return function(...args) {
+        if (!called) {
+            called = true;
+            return fn(...args);
+        }
+        return undefined;
+    };
 }
 
-// Test cases
-console.log(argumentsLength(5)); // 1
-console.log(argumentsLength(5, 10, 15)); // 3
-console.log(argumentsLength("a", "b", "c", "d")); // 4
-console.log(argumentsLength({}, null, "hello", 42, [1,2,3])); // 5
-console.log(argumentsLength()); // 0
+// Test case 1
+const fn = (a, b, c) => a + b + c;
+const onceFn = once(fn);
+
+console.log(onceFn(1, 2, 3)); // 6
+console.log(onceFn(2, 3, 6)); // undefined
+
+// Test case 2 - with different function
+const greet = (name) => `Hello ${name}!`;
+const onceGreet = once(greet);
+
+console.log(onceGreet("Alice")); // "Hello Alice!"
+console.log(onceGreet("Bob"));   // undefined
+console.log(onceGreet("Charlie")); // undefined
+
+// Test case 3 - with no arguments
+const getRandom = () => Math.random();
+const onceRandom = once(getRandom);
+
+const first = onceRandom();
+const second = onceRandom();
+console.log(first); // Some random number
+console.log(second); // undefined
+console.log(first === second); // false (first returns value, second undefined)
